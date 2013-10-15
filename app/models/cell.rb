53a34b94cf2a0a2d0f982eaecd08f43b3325aa49
Bogomir Engel <bogomir.engel@gmx.de>
2013-10-15 17:19:50 +0200

    Made cell kind a symbol field validating the inclusion in a list of types.

--- app/models/cell.rb
+++ app/models/cell.rb
@@ -1,12 +1,18 @@
 class Cell
   include Mongoid::Document
 
-  field :kind, type: String
+  TYPES = [:html, :iframe, :image, :set_link, :text, :title]
+
+  field :kind, type: Symbol
   field :title, type: String
   field :description, type: String
   field :additional_fields, type: Hash, default: {}
 
   mount_uploader :poster_image, ImageUploader
+
+
+  validates_presence_of :title, :kind
+  validates_inclusion_of :kind, in: TYPES
 
   def self.gender
     :m
