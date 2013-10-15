8259312d955f2c0d50bf815b7080019aac66e240
Bogomir Engel <bogomir.engel@gmx.de>
2013-10-15 13:17:52 +0200

    Removed invocation of non-present JS code (not-ported from CMS Scaffold 2).

--- app/assets/javascripts/admins/common.js
+++ app/assets/javascripts/admins/common.js
@@ -1,4 +1,4 @@
-/*global initUploader,initRedactor,setupPublishingWidget,initKalendae,flashTimeout:true */
+/*global setupPublishingWidget,flashTimeout:true */
 
 INIT.addInitializers({
 	common: {
@@ -7,10 +7,7 @@ INIT.addInitializers({
 		},
 
 		_form: function () {
-			initUploader();
-			initRedactor();
 			setupPublishingWidget();
-			initKalendae();
 
 			$(document).on('nested:fieldAdded', initRedactor);
 		}
@@ -22,16 +19,4 @@ function flashTimeout() {
 	setTimeout(function () {
 		$('#flash .alert').alert('close');
 	}, 4000);
-}
-
-
-function initKalendae() {
-	if (document.querySelectorAll) {
-		I18n.initKalendae();
-
-		$('input.kalendae_field').kalendae({
-			weekStart: 1,
-			format: I18n.t('kalendae.dateFormat')
-		});
-	}
 }
