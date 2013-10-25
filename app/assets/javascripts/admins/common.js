bb69cd7685bf1d993c963309156d928f3aa96cad
Alessia Corsini <corsini@meso.net>
2013-10-25 16:22:23 +0200

    Layout improvements case 26747

--- app/assets/javascripts/admins/common.js
+++ app/assets/javascripts/admins/common.js
@@ -18,6 +18,11 @@ INIT.addInitializers({
 			additionalFields.find('.add.btn').click(addEmptyKeyValueFields);
 
 			setupPublishingWidget();
+
+			var collapsableBox = $('.form-inputs').find('.collapsable-box .box-title');
+			if(collapsableBox.length > 0) {
+				initBoxAccordion(collapsableBox);
+			}
 		}
 	}
 });
@@ -27,4 +32,28 @@ function flashTimeout() {
 	setTimeout(function () {
 		$('#flash .alert').alert('close');
 	}, 4000);
+}
+
+function initBoxAccordion(collapsableBox){
+	collapsableBox.each(function(){
+		if($(this).parents('form').hasClass('new_cell_set')) {
+			$(this).parent().addClass('always-open');
+			$(this).find('h3').text('New set');
+		} else {
+			var titleReference = $(this).parent().find('#cell_set_title');
+			$(this).find('h3').text(titleReference.val());
+			titleReference.on('keyup', updateBoxTitle);
+		}
+	});
+	collapsableBox.on('click',createContentAccordion);
+}
+
+function createContentAccordion (){
+	$(this).next('.bubblebox').slideToggle(400);
+	$(this).parent().toggleClass('open');
+}
+
+function updateBoxTitle () {
+	var newTitle = $(this).val();
+	$(this).parents('.collapsable-box').find('h3').text(newTitle);
 }
