c2c5d3a7dbd178beb1a6c00e83a1692bf2e9fcc7
Bogomir Engel <bogomir.engel@gmx.de>
2013-10-29 15:05:58 +0100

    Forcing the generators to use factory_girl.

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,10 @@ Bundler.require(:default, Rails.env)
 
 module MotionBank
   class Application < Rails::Application
+    config.generators do |g|
+      g.test_framework  :test_unit, :fixture_replacement => :factory_girl
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
