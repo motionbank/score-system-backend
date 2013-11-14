681d22c326df97170fdddc4b3e409680d5651eb1
Bogomir Engel <bogomir.engel@gmx.de>
2013-11-14 17:14:39 +0100

    The API output of grid cells includes the poster_image of the grid cell if present, if not it'll be that of the canonical cell.

--- app/helpers/api_helper.rb
+++ app/helpers/api_helper.rb
@@ -7,6 +7,11 @@ module ApiHelper
         self.title = title.presence || cell.title
         self.description = description.presence || cell.description
         self.additional_fields = cell.additional_fields.merge(self.additional_fields)
+
+        # overwrite the poster_image getter to return the cell poster_image if the grid_cell doesn't have one
+        def poster_image
+          super.presence || cell.poster_image
+        end
       end
       grid_cell
     end
