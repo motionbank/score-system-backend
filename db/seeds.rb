8492293bab3e54c15c1c8efe220a5e61ff2f0c04
Florian Jenett <fjenett@gmail.com>
2014-05-23 12:10:27 +0200

    updated gitignore; changed to finer grained ruby version; changed to more generic default database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-email = "admin@meso.net"
+email = "mosys@motionbank.org"
 password = SecureRandom.hex(8)
 unless User.where(email: email).exists?
   User.create!(email: email, password: password, password_confirmation: password, admin: true)
   puts "Created default user #{email} with password #{password}"
 end
 
-score_name = "Deborah Hay"
+score_name = "Example Score"
 unless Score.where(name: score_name).exists?
   score = Score.create!(name: score_name)
   puts "Created default score #{score_name} with URL slug #{score.slug}"
