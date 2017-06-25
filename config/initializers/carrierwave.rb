33f8d473f884cc370a364aadd33d3c8ef432830f
Anton Koch <anton.koch@gmail.com>
2017-06-25 22:20:15 +0200

    removed redundant option for fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@
 CarrierWave.configure do |config|
   config.storage = :fog
 
-  config.fog_provider = 'fog/aws'                        # required
+  # config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
       provider:              'AWS',                        # required
       aws_access_key_id:     ENV['AWS_KEY'],                        # required
