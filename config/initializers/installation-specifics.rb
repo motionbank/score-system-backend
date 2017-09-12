e7e09a0b9491cabe4723f7630b1161124a663930
Florian Jenett <fjenett@gmail.com>
2017-09-12 08:09:48 +0200

    updated frontend path to point to hosteurope (motionbank.org)

--- config/initializers/installation-specifics.rb
+++ config/initializers/installation-specifics.rb
@@ -1,2 +1,3 @@
-# FRONTEND_PATH = "http://scores.wp10610991.server-he.de/".freeze
-FRONTEND_PATH_TEMPLATE = "http://localhost:3331/%{score_id}/public/#set/%{set_id}".freeze
+# FRONTEND_PATH_TEMPLATE = "http://scores.wp10610991.server-he.de/".freeze
+# FRONTEND_PATH_TEMPLATE = "http://localhost:3331/%{score_id}/public/#set/%{set_id}".freeze
+FRONTEND_PATH_TEMPLATE = "http://scores.wp10610991.server-he.de/%{score_id}/public/#set/%{set_id}".freeze
