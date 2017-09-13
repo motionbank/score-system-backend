6a1ab367e5210be7c7fd3902d35785763ea01bf2
Florian Jenett <fjenett@gmail.com>
2017-09-13 18:31:45 +0200

    changes to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-email = "mosys@motionbank.org"
-password = SecureRandom.hex(8)
-unless User.where(email: email).exists?
-  User.create!(email: email, password: password, password_confirmation: password, admin: true)
-  puts "Created default user #{email} with password #{password}"
-end
+# email = "mosys@motionbank.org"
+# password = SecureRandom.hex(8)
+# unless User.where(email: email).exists?
+#   User.create!(email: email, password: password, password_confirmation: password, admin: true)
+#   puts "Created default user #{email} with password #{password}"
+# end
 
-["Pina Bausch", "Sandbox"].each { |score_name|
+["After Care"].each { |score_name|
   unless Score.where(name: score_name).exists?
     score = Score.create!(name: score_name)
     puts "Created default score #{score_name} with URL slug #{score.slug}"
